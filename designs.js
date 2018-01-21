6e3f6c42fd279a5301a8e0fdcbd1ca11c0eec2fb
oomti <oomti@users.noreply.github.com>
2018-01-21 16:14:11 +0000

    Feat: gridMake and coloring functions
    Completed the javascript file, and the basic requirements for the Udacity Pixel Art Maker are fulfilled.

--- designs.js
+++ designs.js
@@ -3,8 +3,54 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
 // Your code goes here!
+//The HTML and CSS file can be left untouched for completing the project
+//The event listener to create the grid
+$('input[type=submit]').on('click',function(event) {
+	
+	//Prevent the form from submitting and page refresh
+	event.preventDefault();
 
+	makeGrid();  
+});
+
+//The event listener to color the pixel
+//Important notice: The table components are created later, so event listeners have to be delegated afterwards.
+
+$('#pixel_canvas').on('click','td',function(event) {
+	const newColor=$('#colorPicker').val(); //Get the color value from the colorPicker input field
+	$(this).css('background-color',newColor);
+})
+
+function makeGrid() {
+	
+	//Delete the previous table
+	$('#pixel_canvas').empty(); 
+
+	//Reading the input field and assigning the value to variables
+	const width=$('#input_width').val();
+	const height=$('#input_height').val();
+		
+	//Creating the table and appending to the canvas
+	// The table could be created without using any for loop, with the string.repeat function of JS
+	for (let row_pos=1;row_pos<=height;row_pos++) {
+		//Creating the rows with unique ID
+		$('#pixel_canvas').append('<tr id="row_'+ row_pos +'"></tr>');
+		for (let col_pos=1;col_pos<=width;col_pos++) {
+			//Creating the table data elements in each row with unique ID
+			$('#row_'+row_pos).append('<td id="row_' + row_pos + '_col_' + col_pos + '"></td>');
+		};
+	};
+	/* //A version with much less function calls
+	let table='<td></td>';
+	table='<tr>'+ table.repeat(width)+'</tr>';
+	table=table.repeat(height);
+	$('#pixel_canvas').append(table);
+	*/
 }
+
+
+
+
+
+
